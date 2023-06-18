a158d73e2c8b8be41db19918aa3f845f5222a35a
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-06-18 18:52:20 +1000

    Test live share commits

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -51,6 +51,7 @@ const App = () => {
 		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
 		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
 		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+		{ name: "Chia-Chuan-Tsou", number: "39-23-6423322", id: 5 },
 	]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
