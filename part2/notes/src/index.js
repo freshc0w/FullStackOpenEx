47a5515423bbb284c215f202c49f393f723fa9a2
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-06-22 17:21:40 +1000

    Complete ex 3.9

--- part2/notes/src/index.js
+++ part2/notes/src/index.js
@@ -4,9 +4,13 @@ import axios from "axios";
 import App from "./App";
 import "./index.css";
 
-axios.get("http://localhost:3001/notes").then((resp) => {
-	const notes = resp.data;
-	ReactDOM.createRoot(document.getElementById("root")).render(
-		<App notes={notes} />
-	);
-});
+// axios.get("http://localhost:3001/notes").then((resp) => {
+// 	const notes = resp.data;
+// 	ReactDOM.createRoot(document.getElementById("root")).render(
+// 		<App notes={notes} />
+// 	);
+// });
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<App />
+)
