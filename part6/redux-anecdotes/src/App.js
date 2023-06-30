0892215fd2576f2af8362b21ac74981fdd2d26ba
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-06-30 18:24:16 +1000

    Complete ex 6.3 and 6.4

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,45 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { voteOnce, createAnec } from './reducers/anecdoteReducer';
+
+const App = () => {
+	const anecdotes = useSelector(state => state);
+	const dispatch = useDispatch();
+
+	const vote = id => {
+		dispatch(voteOnce(id));
+	};
+
+	const addAnec = e => {
+		e.preventDefault();
+		const content = e.target.anec.value;
+		e.target.anec.value = '';
+		dispatch(createAnec(content));
+	};
+
+	return (
+		<div>
+			<h2>Anecdotes</h2>
+			{anecdotes.map(anecdote => (
+				<div key={anecdote.id}>
+					<div>{anecdote.content}</div>
+					<div>
+						has {anecdote.votes}
+						<button onClick={() => vote(anecdote.id)}>vote</button>
+					</div>
+				</div>
+			))}
+			<h2>create new</h2>
+			<form onSubmit={addAnec}>
+				<div>
+					<input
+						type="test"
+						name="anec"
+					/>
+				</div>
+				<button type='submit'>create</button>
+			</form>
+		</div>
+	);
+};
+
+export default App;
