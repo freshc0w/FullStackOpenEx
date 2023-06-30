f8b20ac216ccab0367c452a7ffd2566a10b5986a
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-06-30 21:33:46 +1000

    ex6.10

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+// Not needed anymore:
+// import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 import App from './App'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer,
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer,
+  }
 })
-
-const store = createStore(reducer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,18 +1,31 @@
-const filterReducer = (state = "", action) => {
-    switch(action.type) {
-        case 'SET_FILTER':
-            return action.payload;
+import { createSlice } from '@reduxjs/toolkit';
 
-        default:
-            return state;
-    }
-}
+const filterSlice = createSlice({
+	name: 'filter',
+	initialState: '',
+	reducers: {
+		filterChange(state, action) {
+            console.log('filtering:', state)
+			return action.payload;
+		},
+	},
+});
 
-export const filterChange = filter => {
-    return {
-        type: 'SET_FILTER',
-        payload: filter,
-    }
-}
+export const { filterChange } = filterSlice.actions;
+export default filterSlice.reducer;
 
-export default filterReducer;
+// const filterReducer = (state = '', action) => {
+// 	switch (action.type) {
+// 		case 'SET_FILTER':
+// 			return action.payload;
+
+// 		default:
+// 			return state;
+// 	}
+// };
+// export const filterChange = filter => {
+//     return {
+//         type: 'SET_FILTER',
+//         payload: filter,
+//     }
+// }
