ee4b20aea8005b51ce41d9be6781a8ab5e0d87f8
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-07-11 20:26:45 +1000

    Complete ex 9.8-9.9

---
+++ part9/patientor-backend/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+const app = express();
+import cors from 'cors';
+
+app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+	console.log('someone pinged here');
+	res.send('pong');
+});
+
+app.get('/api/patients', (_req, _res) => {
+	console.log('GET SUCCESS');
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
